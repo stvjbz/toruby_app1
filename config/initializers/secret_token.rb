9859097532c0fdc304a4e8c3f072aa4e84bbbd43
stvjbz <stvjbz@outlook.com>
2014-12-20 15:57:46 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TorubyApp1::Application.config.secret_key_base = '80812832b3411740764229889df5e6b7f3fbf4d0ea2daa6c7ff41864a3a62f0ae9e1735e9daa8b7c228c222688d1ffe049a8cb05331b8511cb1b5c99306b39ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TorubyApp1::Application.config.session_store :cookie_store, key: '_toruby_app1_session'
